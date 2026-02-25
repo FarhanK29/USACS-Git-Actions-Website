b52939a3a557f837e475e2045c6a71526f049528
Farhan Khan <farhan.k0429@gmail.com>
2026-02-25 12:03:20 -0500

    manual deployment of build files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // If your URL is username.github.io/my-repo/, base should be '/my-repo/'
-  // Using './' is a safe 'catch-all' for simple projects
-  base: './', 
-});
